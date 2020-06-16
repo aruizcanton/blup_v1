2b49bfa5025cebc7de4fe8b7b9ff4b600fe338cc
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2020-06-16 11:23:26 +0200

    Introducción de el almacenamiento de la password en caso de que el usuario así lo decida

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:blupv1/app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-
-final storage = FlutterSecureStorage();
 
 void main() => runApp(BlupApp());
