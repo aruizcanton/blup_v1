4dd7d39cd707f34e90d14fbab816451363e9915f
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2020-07-15 13:14:21 +0200

    Funcionalidad del ShoppingCart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:blupv1/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
-void main() => runApp(BlupApp());
+void main(){
+
+  runApp(BlupApp());
+}
